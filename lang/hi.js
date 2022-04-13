9b04c27718cb18c23f489033e6a585ec23c170cd
JenkinsReader <resupport@desire2learn.com>
2022-04-13 07:49:25 -0400

    Translation Updates

--- lang/hi.js
+++ lang/hi.js
@@ -1,8 +1,8 @@
 /* eslint quotes: 0 */
 
 export default {
-	"file_upload_text": "Drag and drop or",
-	"browse": "browse",
-	"browse_files": "Browse Files",
-	"choose_one_file_to_upload": "Choose one file to upload"
+	"file_upload_text": "ड्रैग और ड्रॉप करें या",
+	"browse": "ब्राउज़ करें",
+	"browse_files": "फ़ाइलें ब्राउज़ करें",
+	"choose_one_file_to_upload": "कोई फ़ाइल अपलोड करने के लिए चुनें"
 };
