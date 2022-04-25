b298bc9799817c066fa5d2ab19fc60d0ef0c0314
JenkinsReader <resupport@desire2learn.com>
2022-04-25 11:40:56 -0400

    Translation Updates

--- lang/fr-fr.js
+++ lang/fr-fr.js
@@ -1,8 +1,8 @@
 /* eslint quotes: 0 */
 
 export default {
-	"file_upload_text": "Glisser-déposer ou",
+	"file_upload_text": "Faites glisser et déposez ou",
 	"browse": "parcourir",
 	"browse_files": "Parcourir les fichiers",
-	"choose_one_file_to_upload": "Choisir un fichier à téléverser"
+	"choose_one_file_to_upload": "Sélectionner le fichier à télécharger"
 };
--- lang/nl.js
+++ lang/nl.js
@@ -4,5 +4,5 @@ export default {
 	"file_upload_text": "Sleep en zet neer of",
 	"browse": "blader",
 	"browse_files": "Door bestanden bladeren",
-	"choose_one_file_to_upload": "Kies een bestand dat u wilt uploaden"
+	"choose_one_file_to_upload": "Kies een bestand dat je wilt uploaden"
 };
