15b0d404525a17d79c85ac6e1a066cf9b4ded381
JenkinsReader <resupport@desire2learn.com>
2022-04-29 06:22:58 -0400

    Translation Updates

---
+++ lang/fr-on.js
@@ -0,0 +1,8 @@
+/* eslint quotes: 0 */
+
+export default {
+	"file_upload_text": "Glisser-déposer ou",
+	"browse": "parcourir",
+	"browse_files": "Parcourir les fichiers",
+	"choose_one_file_to_upload": "Choisir un fichier à téléverser"
+};
