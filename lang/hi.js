c5e1ce33de753689901237c3b1ff3d9ec0870365
Ian Wallace <21183583+ian-wallace@users.noreply.github.com>
2022-04-29 18:03:20 -0700

    fix:  US136385 Adding Hindi.

---
+++ lang/hi.js
@@ -0,0 +1,8 @@
+/* eslint quotes: 0 */
+
+export default {
+	"file_upload_text": "ड्रैग और ड्रॉप करें या",
+	"browse": "ब्राउज़ करें",
+	"browse_files": "फ़ाइलें ब्राउज़ करें",
+	"choose_one_file_to_upload": "कोई फ़ाइल अपलोड करने के लिए चुनें"
+};
