c1ededfadc6ba1316acf14c2de023b4e008e94c4
tie.d2l.dev <TieUser@d2l.com>
2024-09-23 14:55:22 +0000

    Translation Updates

--- lang/de.js
+++ lang/de.js
@@ -2,7 +2,7 @@
 
 export default {
 	"file_upload_text": "Mit Drag-and-Drop verschieben oder",
-	"browse": "durchsuchen",
+	"browse": "Durchsuchen",
 	"browse_files": "Dateien durchsuchen",
 	"choose_one_file_to_upload": "Wählen Sie eine Datei zum Hochladen aus"
 };
