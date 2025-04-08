6e1813a54f75b473ae4fffeede9bcb942592896a
tie.d2l.dev <TieUser@d2l.com>
2025-04-08 17:42:58 +0000

    Translation Updates

---
+++ lang/mi.js
@@ -0,0 +1,8 @@
+/* eslint quotes: 0 */
+
+export default {
+	"file_upload_text": "Tō ka whakataka, ka whakataka rānei",
+	"browse": "tirotiro",
+	"browse_files": "Pūtirotirohia ngā kōnae",
+	"choose_one_file_to_upload": "Kōwhiria tētahi kōnae hei tukuatu"
+};
